345156316b90cc4c86ef65e400465cf8ef0bfc05
Sid Meka <gsmekarrow@gmail.com>
2021-12-21 16:15:58 -0500

    Changed a bit.

--- models/superherohead.js
+++ models/superherohead.js
@@ -6,6 +6,7 @@ const SuperHeroHead =new Schema (
         color: { type: color , required: true },
         shape: { type: String , required: true }, //Will use the toLowerCase and === operator //Will have a default shape
         hairtexture: { type: String, required: true}, //Will use the toLowerCase and === operator //Will have a default texture
+        haircolor: { type: color, required: true}, //Will use the toLowerCase and === operator //Will have a default color
     },
     { timestamps: true },
 )
